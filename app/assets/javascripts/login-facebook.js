94c737e363fb3ed4c60ce4b19b2cb1d5170e65fb
zakairobbins <zakairobbins@gmail.com>
2016-02-27 11:30:18 -0800

    add map to happenings/index

--- app/assets/javascripts/login-facebook.js
+++ app/assets/javascripts/login-facebook.js
@@ -1,10 +1,10 @@
-var ref = new Firebase("https://happenin-club.firebaseio.com");
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-  remember: "sessionOnly",
-  scope: "email"
-});
+// var ref = new Firebase("https://happenin-club.firebaseio.com");
+// ref.authWithOAuthPopup("facebook", function(error, authData) {
+//   if (error) {
+//     console.log("Login Failed!", error);
+//   } else {
+//     console.log("Authenticated successfully with payload:", authData);
+//   }
+//   remember: "sessionOnly",
+//   scope: "email"
+// });
--- app/assets/javascripts/login-twitter.js
+++ app/assets/javascripts/login-twitter.js
@@ -1,8 +1,8 @@
-var ref = new Firebase("https://happenin-club.firebaseio.com");
-ref.authWithOAuthPopup("twitter", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+// var ref = new Firebase("https://happenin-club.firebaseio.com");
+// ref.authWithOAuthPopup("twitter", function(error, authData) {
+//   if (error) {
+//     console.log("Login Failed!", error);
+//   } else {
+//     console.log("Authenticated successfully with payload:", authData);
+//   }
+// });
