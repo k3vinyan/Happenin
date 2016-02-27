21d7abc32180fd323c4d5eb0b052799ccddd2bc0
Michael Ashby <ashby.michael@gmail.com>
2016-02-27 14:09:59 -0800

    nothing

---
+++ app/controllers/uber_controller.rb
@@ -0,0 +1,6 @@
+class UberController < ApplicationController
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get '/uber' => 'uber#index'
+
   resources :businesses
   resources :customers
   resources :happenings
