234c5645a277f32d1fb6223fa170f767adcaeb0f
zakairobbins <zakairobbins@gmail.com>
2016-02-27 12:19:04 -0800

    center map on customer location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,33 @@
-  function initMap(){
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-          center: {lat: 44.540, lng: -78.546},
-          zoom: 8
-        });
+function initMap(){
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    zoom: 15
+  });
+  // Try HTML5 geolocation.
+  var pos = {}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      customerMarker.setPosition(pos);
+      // customerMarker.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  var customerMarker = new google.maps.Marker({map: map});
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
