763c7b146fd53d6c851b08f917adea89e291b8e7
Anuja Verma <anuja.verma@gmail.com>
2016-02-27 10:56:22 -0800

    Add authentication using Facebook OAuth

--- app/assets/javascripts/login-facebook.js
+++ app/assets/javascripts/login-facebook.js
@@ -1,10 +1,16 @@
-var ref = new Firebase("https://happenin-club.firebaseio.com");
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-  remember: "sessionOnly",
-  scope: "email"
+$(document).ready(function(){
+    $( "button.facebookAuth" ).on( "click", function( event ) {
+        console.log("Anuja")
+        var ref = new Firebase("http://happenin-club.firebaseio.com");
+        ref.authWithOAuthPopup("facebook", function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+          }
+          // remember: "sessionOnly",
+          // scope: "email"
+        });
+    });
 });
+
--- app/assets/javascripts/login-twitter.js
+++ app/assets/javascripts/login-twitter.js
@@ -1,8 +1,14 @@
-var ref = new Firebase("https://happenin-club.firebaseio.com");
-ref.authWithOAuthPopup("twitter", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
+
+$(document).ready(function(){
+    $( "button.twitterAuth" ).on( "click", function( event ) {
+        console.log("Anuja")
+        var ref = new Firebase("http://happenin-club.firebaseio.com");
+        ref.authWithOAuthPopup("twitter", function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+          }
+        });
+    });
 });
