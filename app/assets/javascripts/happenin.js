5b6510311074da8fe882cf91e124ded7669ae072
zakairobbins <zakairobbins@gmail.com>
2016-02-28 13:01:50 -0800

    add business-address to form and Firebase call for new happenings

--- app/assets/javascripts/happenin.js
+++ app/assets/javascripts/happenin.js
@@ -1,20 +1,19 @@
 // Write data to Firebase
 
 $(document).ready(function(){
-    $("#new_happenin").on("click", function( event ) {
-      var title = $('#title_input').val()
-      var description = $('#description_input').val()
-      var startTime = $('#start_time_input').val()
-      var endTime = $('#end_time_input').val()
-      var businessID = $('#business-id').val()
-      event.preventDefault();
+  $("#new_happenin").on("click", function( event ) {
+    var title = $('#title_input').val()
+    var description = $('#description_input').val()
+    var startTime = $('#start_time_input').val()
+    var endTime = $('#end_time_input').val()
+    var businessID = $('#business-id').val()
+    var businessAddress = $('#business-address').val()
+    event.preventDefault();
 
-        var myFirebaseRef = new Firebase("https://happenin-club.firebaseio.com");
+    var myFirebaseRef = new Firebase("https://happenin-club.firebaseio.com");
 
-        myFirebaseRef.child("happenin").child(businessID).push({title: title, description: description, startTime: startTime, endTime: endTime})
-    });
-
-
+    myFirebaseRef.child("happenin").child(businessID).push({title: title, description: description, businessAddress: businessAddress, startTime: startTime, endTime: endTime})
+  });
 });
 
 // Retrieve data from Firebase
