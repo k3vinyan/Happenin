c98ac00a07d7d1e3ab2692795ff08d0c6d2bdb85
Kevin Yan <k3vinyan@gmail.com>
2016-02-28 12:18:15 -0800

    stylize the splash page buttons and create back buttons

--- app/assets/javascripts/business.js
+++ app/assets/javascripts/business.js
@@ -1,67 +1,22 @@
-  // $(document).ready(function(){
-
-  // $('.leftContent').bind('DOMSubtreeModified', function(e) {
-  //   $('.busSubmittButton').on('click', function(e){
-  //     e.preventDefault();
-  //     $.ajax({
-  //       type: 'get',
-  //       url: '/businesses/<new></new>',
-  //       success: function(data){
-  //        console.log(data)
-  //       },
-  //       error: function(xhr, options, error){
-  //         console.log(xhr.status);
-  //         console.log(error);
-  //       }
-  //     })
-  //   })
-
-  //   $('.showContent').on('click', function(e){
-  //     e.preventDefault()
-  //       $(".leftContent").empty();
-  //         $(".leftContent").removeClass('bottomDiv')
-  //         $(".leftContent").html(data);
-  //   })
-
-  // });
+$(document).ready(function(){
+  $('.leftContent').bind('DOMSubtreeModified', function(e) {
+     $(".backButton").on('click', function(e){
+      e.preventDefault();
+      $.ajax({
+        type: 'get',
+        url: '/welcome/login',
+        success: function(data){
+         $(".leftContent").empty();
+          $(".leftContent").removeClass('bottomDiv')
+          $(".leftContent").html(data);
+        },
+        error: function(xhr, options, error){
+          console.log(xhr.status);
+          console.log(error);
+        }
+      })
+    })
+  })
+})
 
 
-// $(document).ready(function(){
-//   $('.leftContent').bind('DOMSubtreeModified', function(e) {
-//     $('.busSubmittButton').on('click', function(e){
-//       console.log("cat")
-//       e.preventDefault();
-//       $.ajax({
-//         type: 'get',
-//         url: '/businesses/show',
-//         success: function(data){
-//          console.log(data)
-//         },
-//         error: function(xhr, options, error){
-//           console.log(xhr.status);
-//           console.log(error);
-//         }
-//       })
-//     })
-//   });
-    // $('.leftContent').bind('DOMSubtreeModified', function(e) {
-    //   $('.createDealsButton').on('click', function(e){
-    //     e.preventDefault();
-    //     $.ajax({
-    //     type: 'post',
-    //     data : {
-    //       partion : 'create' // will be accessible in $_POST['data1']
-    //     },
-    //     dataType: 'json',
-    //     url: '/businesses/show',
-    //     success: function(data){
-    //      console.log(data)
-    //     },
-    //     error: function(xhr, options, error){
-    //       console.log(xhr.status);
-    //       console.log(error);
-    //     }
-    //   })
-    // }
-
-// })
